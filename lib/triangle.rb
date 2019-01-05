91b7f8d7051e8915fd1bb2612b4f2e1cb1fe4059
Alison Reilly <reillyamr@gmail.com>
2019-01-05 02:48:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z 
+  
+  def initialize(x, y, z) 
+    @x = x 
+    @y = y 
+    @z = z 
+  end 
+  
+  def kind 
+    
+    if x 
+    
+    
+    :equilateral 
+    :isosceles 
+    :scalene
+    
+  end 
+  
+  class TriangleError < StandardError
+    
+  end 
+    
+  
+  
 end
